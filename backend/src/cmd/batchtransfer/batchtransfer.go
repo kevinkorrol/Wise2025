a99750a004d36de0022f141f4ea67c3c6d269fd8
Nivis <marten.mehide@mits.ee>
2025-11-28 17:05:40 +0200

    Backend docker

--- backend/src/cmd/batchtransfer/batchtransfer.go
+++ backend/src/cmd/batchtransfer/batchtransfer.go
@@ -3,7 +3,7 @@ package main
 import app2 "github.com/kevinkorrol/Wise2025/internal/app"
 
 func main() {
-	app := app2.New("127.0.0.1:8080")
+	app := app2.New(":8080")
 	app.CreateMockUsers()
 	app.Start()
 }
